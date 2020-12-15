a57814197ff1c7cef3207c8efe99132ba0945d3a
LYwinnie <1366782080@qq.com>
2020-12-15 23:28:19 +0800

    删除空行

--- src/index.js
+++ src/index.js
@@ -4,7 +4,6 @@ import  "core-js/es";
 import  "mutation-observer";
 import  "react-app-polyfill/ie9";
 import  "react-app-polyfill/stable";
-
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
