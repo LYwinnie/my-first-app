290d0ab17ba135a0a1db0a63f73ead28def7a33f
LYwinnie <1366782080@qq.com>
2021-01-27 22:45:41 +0800

    feat:修改4

--- src/page/index.jsx
+++ src/page/index.jsx
@@ -21,6 +21,7 @@ class App extends React.Component {
                   //主要是为了传递嵌套路由到子组件 feature-liuyan2
                   //主要是为了传递嵌套路由到子组件main
                   //主要是为了传递嵌套路由到子组件main3
+                  //主要是为了传递嵌套路由到子组件main4
                   //类似于 <User {...props} routes={routes} />
                   <item.component {...props} routers={item.children} />
 
