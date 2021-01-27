ce3dbb71b8c2ab73207f45d96fe9152536282706
LYwinnie <1366782080@qq.com>
2021-01-28 00:22:29 +0800

    1

--- src/page/index.jsx
+++ src/page/index.jsx
@@ -17,13 +17,6 @@ class App extends React.Component {
               return <Route key={item.name} path={item.path} exact={item.exact}
                 render={props => (
                   //主要是为了传递嵌套路由到子组件 
-                  //主要是为了传递嵌套路由到子组件 feature-liuyan
-                  //主要是为了传递嵌套路由到子组件 feature-liuyan2
-                  //主要是为了传递嵌套路由到子组件main
-                  //主要是为了传递嵌套路由到子组件main3
-                  //主要是为了传递嵌套路由到子组件main4
-                  //主要是为了传递嵌套路由到子组件main5
-                  //主要是为了传递嵌套路由到子组件main6
                   //类似于 <User {...props} routes={routes} />
                   <item.component {...props} routers={item.children} />
 
